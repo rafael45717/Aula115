8dac636276f8b45bac7e7979f1369aac59976632
rafael45717 <120226885+rafael45717@users.noreply.github.com>
2024-04-16 15:43:27 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+let video;
+let poseNet;
+
+function setup() {
+  createCanvas(400, 400);
+  video = createCapture(VIDEO);
+  video.hide();
+  poseNet = ml5.poseNet(video, modeloCarregado);
+  poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(poses) {
+  console.log(poses)
+  if(poses.lenght > 0){
+    pose = poses[0].pose;
+  }
+}
+
+function modeloCarregado() {
+  console.log("Modelo carregado!")
+}
+
+function draw() {
+  background(220);
+  image(video, 0, 0);
+  fill(255,0,0);
+  circle(pose.nose.x, pose.nose.y, 30);
+}
+
+function takeSnapshot() {
+  save('snapshot.png')
+
+
+}
+
+
